3cc4a281027e707690cc270673051013ad181816
alfred-jhc <ajh@clark.moe>
2015-03-24 18:31:54 +0000

    normalized RGB values

--- PCCode/sdp/vision/PixelInfo.java
+++ PCCode/sdp/vision/PixelInfo.java
@@ -15,13 +15,20 @@ public class PixelInfo {
 	
 	public int column,row; // Position of pixel in frame
 	public int r,g,b; // RGB values of pixel in frame
+	public int rgb; // total of RGB values
 	public float h,s,v; // HSV values of pixel in frame
 	
 	public PixelInfo(Color c){
 		this.r = c.getRed();
 		this.g = c.getGreen();
 		this.b = c.getBlue();
-		
+		this.rgb = r+g+b;
+
+		//TODO Normalize
+		this.r = (r * 255) / rgb;
+		this.g = (g * 255) / rgb;
+		this.b = (b * 255) / rgb;
+
 		float[] hsvals = Color.RGBtoHSB(r, g, b, null);
 		this.h = hsvals[0];
 		this.s = hsvals[1];
